e1e648a998c32c94646f623d6adc89c7629a8e51
Kasidate444 <kasidatesaeeaw@gmail.com>
2026-03-10 16:36:14 +0700

    seventh edited 06

--- 06_bring-it-to-javascript.js
+++ 06_bring-it-to-javascript.js
@@ -1,57 +1,58 @@
-let produtList = [eagle_food, snake_food, dragon_food, eagle_leash, snake_leash, dragon_leash];
-
-const all_product = {
-    productId: 0,
-    productName: "eagle food",
-    price: 50,
-
-    productId: 1,
-    productName: "snake food",
-    price: 60,
-
-    productId: 2,
-    productName: "dragon food",
-    price: 40,
-
-    productId: 3,
-    productName: "eagle leash",
-    price: 100,
-
-    productId: 4,
-    productName: "snake leash",
-    price: 90,
-
-    productId: 5,
-    productName: "dragon leash",
-    price: 80,
-}
-
-let data = [
-    {
-        productId: 0,
-        quantity:10,
-    },
-    {
-        productId: 1,
-        quantity:10,
-    },
-    {
-        productId: 2,
-        quantity:10,
-    },
-    {
-        productId: 3,
-        quantity:10,
-    },
-    {
-        productId: 4,
-        quantity:10,
-    },
-    {
-        productId: 5,
-        quantity:10,
-    },
+let productList = [
+    "eagle food",
+    "snake food",
+    "dragon food",
+    "eagle leash",
+    "snake leash",
+    "dragon leash"
 ];
 
-console.log(produtList);
+const all_product = [
+    {
+        productId: 0,
+        productName: "eagle food",
+        price: 50
+    },
+    
+    {
+        productId: 1,
+        productName: "snake food",
+        price: 60
+    },
+    
+    {
+        productId: 2,
+        productName: "dragon food",
+        price: 40
+    },
+    
+    {
+        productId: 3,
+        productName: "eagle leash",
+        price: 100
+    },
+    
+    {
+        productId: 4,
+        productName: "snake leash",
+        price: 90
+    },
+    
+    {
+        productId: 5,
+        productName: "dragon leash",
+        price: 80
+    }
+];
+
+let data = [
+    { productId: 0, quantity: 10 },
+    { productId: 1, quantity: 10 },
+    { productId: 2, quantity: 10 },
+    { productId: 3, quantity: 10 },
+    { productId: 4, quantity: 10 },
+    { productId: 5, quantity: 10 }
+];
+
+console.log(productList);
 console.log(all_product);
