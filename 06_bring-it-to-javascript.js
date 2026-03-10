f80de59040372cba60e813fab69a0dd95918f448
Kasidate444 <kasidatesaeeaw@gmail.com>
2026-03-10 15:41:49 +0700

    second add 0406 not empty

---
+++ 06_bring-it-to-javascript.js
@@ -0,0 +1,57 @@
+let produtList = [eagle_food, snake_food, lizard_food, eagle_leash, snake_leash, lizard_leash];
+
+const all_product = {
+    productId: 0,
+    productName: "eagle food",
+    price: 50,
+
+    productId: 1,
+    productName: "snake food",
+    price: 60,
+
+    productId: 2,
+    productName: "lizard food",
+    price: 40,
+
+    productId: 3,
+    productName: "eagle leash",
+    price: 100,
+
+    productId: 4,
+    productName: "snake leash",
+    price: 90,
+
+    productId: 5,
+    productName: "lizard leash",
+    price: 80,
+}
+
+let data = [
+    {
+        productId: 0,
+        quantity:10,
+    },
+    {
+        productId: 1,
+        quantity:10,
+    },
+    {
+        productId: 2,
+        quantity:10,
+    },
+    {
+        productId: 3,
+        quantity:10,
+    },
+    {
+        productId: 4,
+        quantity:10,
+    },
+    {
+        productId: 5,
+        quantity:10,
+    },
+];
+
+console.log(produtList);
+console.log(all_product);
